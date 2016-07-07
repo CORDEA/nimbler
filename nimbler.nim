cd1a2d4adfa31aac6083ed4673fb65273733291c
Yoshihiro Tanaka <contact@cordea.jp>
2016-07-07 19:12:21 +0900

    isolate methods, use modules

--- nimbler.nim
+++ nimbler.nim
@@ -14,169 +14,154 @@
 # Author: Yoshihiro Tanaka <contact@cordea.jp>
 # date  :2016-06-23
 
-import os
-import httpclient, parseopt2
-import pegs, strutils, subexes, json, sequtils
+import os, parseopt2
+import pegs, subexes
+import httpclient, json
+import strutils, sequtils
+import searchresp, fileenv, lib
 
 const
     defaultRepo = "nim-lang/packages"
-    nimbleBaseDir = getEnv("HOME") / "/.nimble/"
+    reposFileName = "repos$#.tsv"
     configFileDir = "/nimbler/"
-    dbListFileName = "repos$#.txt"
     nimbleFileName = "packages$#.json"
-    libFileName = "$#.nimble"
-    tmpFilePath = "_tmp"
     apiBaseUrl = "https://api.github.com"
     rawBaseUrl = "https://raw.githubusercontent.com/$#/master$#"
     searchCodeApiPath = "/search/code?q=$#+in:path+extension:$#+repo:$#"
     httpStatusOk = "200 OK"
-    test = true
 
-type
-    Lib = object
-        name, url, meth, desc, license, web: string
-        tags: seq[string]
-
-proc getFmtDbListFileName(arg: string): string =
-    let path = nimbleBaseDir / configFileDir
-    if not path.existsDir:
-        path.createDir()
-    return path / (subex(dbListFileName) % [arg])
-
-proc getTmpDbListFileName(): string =
-    return getFmtDbListFileName(tmpFilePath)
-
-proc getDbListFileName(): string =
-    return getFmtDbListFileName("")
-
-proc getFmtNimbleFileName(arg: string): string =
-    return nimbleBaseDir / (subex(nimbleFileName) % [arg])
-
-proc getTmpNimbleFileName(): string =
-    return getFmtNimbleFileName(tmpFilePath)
-
-proc getNimbleFileName(): string =
-    return getFmtNimbleFileName("")
+let
+    reposEnv = initFileEnv(reposFileName, configFileDir)
+    nimbleEnv = initFileEnv(nimbleFileName, "")
 
 proc getTmpDbListFile(): File =
-    result = getTmpDbListFileName().open(fmWrite)
+    result = reposEnv.absTmpPath.open(fmWrite)
 
 proc getDbListFile(): File =
-    if getDbListFileName().existsFile:
-        result = getDbListFileName().open(fmRead)
+    let fileName = reposEnv.absPath
+    if fileName.existsFile:
+        result = fileName.open(fmRead)
+
+proc getDbFilePath(fullName: string): string =
+    return nimbleEnv.getAbsFmtPath("_" & fullName.replace("/", "-"), configFileDir)
 
 proc overwriteDbListFile(): bool =
-    result = getTmpDbListFileName().existsFile
+    let fileName = reposEnv.absTmpPath
+    result = fileName.existsFile
     if result:
-        getTmpDbListFileName().moveFile getDbListFileName()
-
-proc `<>`(node: JsonNode, q: string): string =
-    return if node.hasKey(q): node[q].str else: nil
-
-proc `%`(lib: Lib): JsonNode =
-    result = newJObject()
-    for k, v in lib.fieldPairs:
-        var key = k
-        if key == "meth":
-            key = "method"
-        if v != nil:
-            result[key] = %v
-
-proc toData(node: JsonNode): Lib =
-    var sq: seq[string] = @[]
-    if node.hasKey("tags"):
-        for n in node["tags"]:
-            sq.add n.str
-    result = Lib(
-        name: node<>"name",
-        url: node<>"url",
-        meth: node<>"method",
-        desc: node<>"description",
-        license: node<>"license",
-        web: node<>"web",
-        tags: sq
-    )
-
-proc downloadDbFile(fullName, path: string): string =
-    let
-        rawUrl = subex(rawBaseUrl) % [fullName, path]
-        res = get(rawUrl)
-    if res.status == httpStatusOk:
-        result = res.body
+        fileName.moveFile reposEnv.absPath
 
 proc searchFile(q, ext, repo: string): JsonNode =
-    if test:
-        return parseJson("res-test.json".readFile())
     let
         url = apiBaseUrl & subex(searchCodeApiPath) % [q, ext, repo]
         res = get(url)
     if res.status == httpStatusOk:
         return parseJson(res.body)
 
-proc mergeFile(targ: JsonNode, isOverwrite: bool): string =
-    var
-        libs: seq[Lib] = @[]
-        deduplicates: seq[Lib] = @[]
-    if isOverwrite:
-        let body = getNimbleFileName().readFile()
-        for j in parseJson(body):
-            libs.add toData(j)
-    for t in targ:
-        libs.add toData(t)
-    for lib in libs:
-        if len(deduplicates.filter(proc(x: Lib): bool = x.name == lib.name)) == 0:
-            deduplicates.add lib
-    result = (%deduplicates).pretty
+proc mergeFile(src, trg: JsonNode): string =
+    var libs: seq[Lib] = @[]
+    if src != nil:
+        for t in src:
+            libs.add toLib(t)
+    if trg != nil:
+        for t in trg:
+            libs.add toLib(t)
+    result = deduplicate(libs).pretty()
 
-proc fetchDbFile(repos: seq[string], isOverwrite: bool): bool =
-    let writef = getTmpNimbleFileName().open(fmWrite)
-    if not getNimbleFileName().existsFile:
-        stderr.writeLine(getNimbleFileName() & " does not exist.")
-        quit 1
-
-    let readf = getNimbleFileName().open(fmRead)
-    defer: close(readf)
+proc downloadDbFile(resp: SearchResponse): bool =
+    let
+        path = getDbFilePath(resp.fullName)
+        writef = path.open(fmWrite)
+        rawUrl = subex(rawBaseUrl) % [resp.fullName, resp.path]
+        res = get(rawUrl)
 
     result = false
+    if res.status == httpStatusOk:
+        let json = parseJson res.body
+        writef.write json
+        writef.close()
+        result = true
+
+proc mergeDbFile(req: seq[SearchResponse], isOverwrite: bool): bool =
+    let
+        path = nimbleEnv.absPath
+        tmpPath = nimbleEnv.absTmpPath
+
+    if isOverwrite:
+        path.copyFile tmpPath
+    else:
+        if tmpPath.existsFile:
+            tmpPath.removeFile()
+
+    result = false
+    if len(req) == 0:
+        let writef = tmpPath.open(fmWrite)
+        writef.write "[]"
+        writef.close()
+    else:
+        for r in req:
+            let
+                src = if tmpPath.existsFile: parseJson tmpPath.readFile() else: nil
+                trg = parseJson getDbFilePath(r.fullName).readFile()
+                writef = tmpPath.open(fmWrite)
+            writef.write mergeFile(src, trg)
+            writef.close()
+    result = tmpPath.existsFile
+    if result:
+        tmpPath.moveFile path
+
+proc searchDbFile(repos: seq[string]): seq[SearchResponse] =
+    result = @[]
     for line in repos:
-        var
-            jn = searchFile(nimbleFileName, "json", line)
+        let
+            jn = searchFile(nimbleEnv.fileName, "json", line)
+        if jn == nil:
+            result.add nil
+            continue
+        let
             its = jn["items"]
         if len(its) == 1:
             let
                 path = its[0]["path"].str
                 fullName = its[0]["repository"]["full_name"].str
-            let body = downloadDbFile(fullName, path)
-            if body != nil:
-                let json = parseJson body
-                writef.write mergeFile(json, isOverwrite)
-                writef.close()
-                result = getTmpNimbleFileName().existsFile
-                if result:
-                    getTmpNimbleFileName().moveFile getNimbleFileName()
+                resp = SearchResponse(path: path, fullName: fullName)
+            result.add resp
 
 proc updateRepo(): bool =
     let readf = getDbListFile()
+    if readf == nil:
+        return true
     defer: readf.close()
-    var cont = readf.readAll().splitLines
-    let existDefRepo = len(cont.filter(proc (x: string): bool = x == defaultRepo)) > 0
 
-    if not existDefRepo:
-        cont.add defaultRepo
-    result = fetchDbFile(cont, false)
+    let sr = readf.readAll().toSearchResponses()
+    for r in sr:
+        if r != nil and downloadDbFile(r):
+            echo r.fullName & " update completed."
+        else:
+            stderr.writeLine r.fullName & " update failure."
+    result = mergeDbFile(sr, false)
 
 proc registerRepo(repo: string): bool =
     let
         writef = getTmpDbListFile()
         readf = getDbListFile()
+    var repos: seq[string] = @[repo]
     defer: writef.close()
-    defer: readf.close()
-    if readf != nil:
+    if readf == nil:
+        if repo != defaultRepo:
+            repos.add defaultRepo
+    else:
+        defer: readf.close()
         for line in readf.readAll().splitLines:
-            if $line == repo:
-                return
-    if fetchDbFile(@[repo], true):
-        writef.writeLine repo
+            if line.split("\t")[0] == repo:
+                echo repo & " already exists."
+                quit 0
+
+    let sr = searchDbFile(repos)
+    for r in sr:
+        if r != nil and downloadDbFile(r):
+            writef.writeLine(r.fullName & "\t" & r.path)
+    discard mergeDbFile(sr, true)
     result = overwriteDbListFile()
 
 proc removeRepo(repo: string): bool =
@@ -184,16 +169,25 @@ proc removeRepo(repo: string): bool =
     let
         writef = getTmpDbListFile()
         readf = getDbListFile()
+    if readf == nil:
+        return true
     defer: readf.close()
     defer: writef.close()
+
+    var repos: seq[SearchResponse] = @[]
+
     for line in readf.readAll().splitLines:
-        if $line == "":
+        if line == "":
             continue
-        if $line == repo:
+        let resp = line.toSearchResponse()
+        if resp.fullName == repo:
             result = true
             continue
+        repos.add resp
         writef.writeLine line
+
     discard overwriteDbListFile()
+    discard mergeDbFile(repos, false)
 
 when isMainModule:
     var
@@ -210,6 +204,10 @@ when isMainModule:
                 sarg = key
         else: discard
         inc i
+
+    if sub == nil:
+        stderr.writeLine "Please specify a sub-command."
+        quit 1
 
     case sub
     of "add":
